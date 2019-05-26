211502965658d3a5c409adedbf30e11669445f3c
Gelehrte Crest <ein.gelehrte@gmail.com>
2019-05-27 02:28:59 +0900

    UIなどたたき台

--- docs/main.js
+++ docs/main.js
@@ -30,16 +30,29 @@
 		img.font = $('#style').val() + ' ' + $('#px').val() + $('#font').val();
 
 		//合成画像の設定
+		//回転
+		img.rotation = imageIni.rotation;
+		//回転の中心は、画像の中央
+		img.regX = img.getBounds().width / 2;
+		img.regY = img.getBounds().height / 2;
 		//上下は10ピクセルごと移動
 		img.x = imageIni.xPos * 10;
 		img.y = imageIni.yPos * 10;
+		console.log(img.x);
+		console.log(img.y);
 		//拡縮は10％ずつ
 		img.scaleX = img.scaleX * (1 + imageIni.Scale / 10);
 		img.scaleY = img.scaleY * (1 + imageIni.Scale / 10);
 
+		//透明化
+		img.alpha = imageIni.alpha;	
+	
 		//影
-		var shadow = new createjs.Shadow("#cb8a00", 0, 0, 20);
-		img.shadow = shadow;
+		try{
+			var shadow = new createjs.Shadow($('#color_shadow').val(), 4, 4, imageIni.shadow);
+			img.shadow = shadow;
+		} catch(e){
+		}
 
 		//ステージ生成
 		stage.addChild(img2);
@@ -53,15 +66,19 @@
 		//設定のデフォルト値
 		$('#text').val('文字入れジェネレータ');
 		$('#color').val('white');
+		$('#color_shadow').val('#cb8a00');
 		$('#style').val('bold');
 		$('#font').val("/1.5 'ヒラギノ明朝 ProN','Hiragino Mincho ProN','Sawarabi Mincho','Noto Serif CJK JP','MS PMincho',serif");
 		$('#px').val('75px');
 
 		//読込画像のオブジェクト
 		var imageIni = {
-			xPos : 8,
-			yPos : 8,
+			xPos : 80,
+			yPos : 20,
 			Scale : 8,
+			rotation : 0,
+			alpha : 1.0,
+			shadow : 40,
 			imageData : null,
 			logoImageData : null,
 			resetImage : function(){
@@ -141,6 +158,24 @@
 				imageIni.Scale += 1;
 			}else if (e.target.id === 'zoomout') {
 				imageIni.Scale -= 1;
+			}else if (id === 'rotation_r') {
+				imageIni.rotation += 7.5;
+			}else if (id === 'rotation_l') {
+				imageIni.rotation -= 7.5;
+			}else if (id === 'alpha_up') {
+				imageIni.alpha += 0.1;
+				if(imageIni.alpha >= 0.9){
+					imageIni.alpha = 1.0;
+					$('#alpha_up').prop("disabled", true);
+				}
+				$('#alpha_down').prop("disabled", false);
+			}else if (id === 'alpha_down') {
+				imageIni.alpha -= 0.1;
+				if(imageIni.alpha <= 0.1){
+					imageIni.alpha = 0.0;
+					$('#alpha_down').prop("disabled", true);
+				}
+				$('#alpha_up').prop("disabled", false);
 			}else if (e.target.id === 'reset'){
 				imageIni.resetImage();
 			}else if (e.target.id === 'dl'){
@@ -170,9 +205,127 @@
 			write_settingurl(imageIni);
 		});
 
+		//ボタンイベントまとめ
+		var editgenerator_button = "";
+		var flag = 0;
+		function editgenerator(id){
+			if(flag == 0){
+				flag = 1;
+				return;
+			} else {
+				flag = 0;
+			}
+			if (id === 'update'){
+			}else if (id === 'up'){
+				imageIni.yPos -= 1;
+			}else if (id === 'down'){
+				imageIni.yPos += 1;
+			}else if (id === 'left'){
+				imageIni.xPos -= 1;
+			}else if (id === 'right') {
+				imageIni.xPos += 1;
+			}else if (id === 'zoomin') {
+				imageIni.Scale += 1;
+			}else if (id === 'zoomout') {
+				imageIni.Scale -= 1;
+			}else if (id === 'rotation_r') {
+				imageIni.rotation += 7.5;
+			}else if (id === 'rotation_l') {
+				imageIni.rotation -= 7.5;
+			}else if (id === 'alpha_up') {
+				imageIni.alpha += 0.1;
+				if(imageIni.alpha >= 0.9){
+					imageIni.alpha = 1.0;
+					$('#alpha_up').prop("disabled", true);
+				}
+				$('#alpha_down').prop("disabled", false);
+			}else if (id === 'alpha_down') {
+				imageIni.alpha -= 0.1;
+				if(imageIni.alpha <= 0.1){
+					imageIni.alpha = 0.0;
+					$('#alpha_down').prop("disabled", true);
+				}
+				$('#alpha_up').prop("disabled", false);
+			}else if (id === 'reset'){
+				imageIni.resetImage();
+				$('#alpha_up').prop("disabled", true);
+				$('#alpha_down').prop("disabled", false);
+			}else if (id === 'dl'){
+				return;
+			}
+
+			//画像操作時は再描画を行う
+			if(imageIni.imageData !== null){
+				$('#alert').text('合成作業開始中です。');
+				imageIni.makeImage();
+				$('#alert').text('合成完了です！');
+			}else{
+				$('#alert').text('スクリーンショットを入力してから画像生成を行ってください');
+			}
+
+			//画面操作時はURLを再生成する
+			write_settingurl(imageIni);
+		}
+		//$('.btn').on('click', function(e){
+		//	editgenerator_button = e.target.id;
+		//	editgenerator(editgenerator_button);
+		//});
+
+		var pushing_flag = 0;
+		var mouse_push_hold = function(){
+			editgenerator(editgenerator_button);
+			if( pushing_flag == 1 ){
+				setTimeout(mouse_push_hold, 100);
+			}
+		};
+
+		// PC用
+		$(".editgenerator").mousedown(function(e){
+			editgenerator_button = e.target.id;
+			pushing_flag = 1;
+			setTimeout(mouse_push_hold, 1);
+			return false;
+		}).mouseup(function(){
+			pushing_flag = 0;
+			clearTimeout(mouse_push_hold);
+		}).mouseleave(function(){
+			pushing_flag = 0;
+			clearTimeout(mouse_push_hold);
+		}).mouseover(function(){
+			pushing_flag = 0;
+			clearTimeout(mouse_push_hold);
+		});
+
+		//スマホ用
+		$(".editgenerator").bind('touchstart', function(e){
+			editgenerator_button = e.target.id;
+			pushing_flag = 1;
+			setTimeout(mouse_push_hold, 1);
+			return false;
+		});
+		$(".editgenerator").bind('touchend', function(e){
+			pushing_flag = 0;
+			return false;
+		});
+
+
+
+
+		$('input[name=logo]').click(function() {
+			//チェックボックス操作時は再描画を行う
+			if(imageIni.imageData !== null){
+				imageIni.makeImage();
+			}else{
+				$('#alert').text('スクリーンショットを入力してから画像生成を行ってください');
+			}
+
+			//チェックボックス操作時はURLを再生成する
+			write_settingurl(imageIni);
+		});
+
 		//初回URL生成
 		write_settingurl(imageIni);
-	
+
 		//Canvas Download
 		$('#btnDownload').on("click", function() {
 			$('#alert').text('ダウンロード ボタンクリック');
