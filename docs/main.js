d75d48179f57cc999263711f71395542c2583026
Gelehrte Crest <ein.gelehrte@gmail.com>
2021-01-21 05:28:32 +0900

    fix

--- docs/main.js
+++ docs/main.js
@@ -2,7 +2,7 @@
 	// フォントロード
 	WebFont.load({
 		custom: {
-			families: ['Norvrandt','Kugane', '@Kugane','NieR','Eorzea', 'Allagan', 'Near-Eastern', 'Eden', 'Eorzea-Blackletter','TrainOne','Stick','RocknRollOne','Reggae','RampartOne','KleeOne','KleeOne-SemiBold','DotGothic16'],
+			families: ['Norvrandt','Kugane', '@Kugane','NieR','Eorzea', 'Allagan', 'Near-Eastern', 'Eden', 'Eorzea-Blackletter','TrainOne','Stick','RocknRollOne','ReggaeOne','RampartOne','KleeOne','KleeOne-SemiBold','DotGothic16'],
 			urls: ['style.css']
 		},
 		loading: function() {
@@ -47,7 +47,7 @@
 	const font_trainone = "/1.5 'TrainOne','ヒラギノ明朝 ProN','Hiragino Mincho ProN','Sawarabi Mincho','Noto Serif CJK JP','MS PMincho',serif";
 	const font_stick = "/1.5 'Stick','ヒラギノ明朝 ProN','Hiragino Mincho ProN','Sawarabi Mincho','Noto Serif CJK JP','MS PMincho',serif";
 	const font_rokenrollone = "/1.5 'RocknRollOne','ヒラギノ明朝 ProN','Hiragino Mincho ProN','Sawarabi Mincho','Noto Serif CJK JP','MS PMincho',serif";
-	const font_reggae = "/1.5 'Reggae','ヒラギノ明朝 ProN','Hiragino Mincho ProN','Sawarabi Mincho','Noto Serif CJK JP','MS PMincho',serif";
+	const font_reggaeone = "/1.5 'ReggaeOne','ヒラギノ明朝 ProN','Hiragino Mincho ProN','Sawarabi Mincho','Noto Serif CJK JP','MS PMincho',serif";
 	const font_rampartone = "/1.5 'RampartOne','ヒラギノ明朝 ProN','Hiragino Mincho ProN','Sawarabi Mincho','Noto Serif CJK JP','MS PMincho',serif";
 	const font_kleeone = "/1.5 'KleeOne','ヒラギノ明朝 ProN','Hiragino Mincho ProN','Sawarabi Mincho','Noto Serif CJK JP','MS PMincho',serif";
 	const font_kleeonesemibold = "/1.5 'KleeOne-SemiBold','ヒラギノ明朝 ProN','Hiragino Mincho ProN','Sawarabi Mincho','Noto Serif CJK JP','MS PMincho',serif";
@@ -805,8 +805,8 @@
 				$('#font').val(font_stick);
 				} else if(id == "font_rokenrollone"){
 				$('#font').val(font_rokenrollone);
-				} else if(id == "font_reggae"){
-				$('#font').val(font_reggae);
+				} else if(id == "font_reggaeone"){
+				$('#font').val(font_reggaeone);
 				} else if(id == "font_rampartone"){
 				$('#font').val(font_rampartone);
 				} else if(id == "font_kleeone"){
@@ -847,8 +847,8 @@
 				$('#fontB').val(font_stick);
 				} else if(id == "font_rokenrolloneB"){
 				$('#fontB').val(font_rokenrollone);
-				} else if(id == "font_reggaeB"){
-				$('#fontB').val(font_reggae);
+				} else if(id == "font_reggaeoneB"){
+				$('#fontB').val(font_reggaeone);
 				} else if(id == "font_rampartoneB"){
 				$('#fontB').val(font_rampartone);
 				} else if(id == "font_kleeoneB"){
@@ -887,8 +887,8 @@
 				$('#fontC').val(font_stick);
 				} else if(id == "font_rokenrolloneC"){
 				$('#fontC').val(font_rokenrollone);
-				} else if(id == "font_reggaeC"){
-				$('#fontC').val(font_reggae);
+				} else if(id == "font_reggaeoneC"){
+				$('#fontC').val(font_reggaeone);
 				} else if(id == "font_rampartoneC"){
 				$('#fontC').val(font_rampartone);
 				} else if(id == "font_kleeoneC"){
